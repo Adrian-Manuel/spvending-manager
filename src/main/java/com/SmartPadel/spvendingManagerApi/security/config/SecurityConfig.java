839a3023ece37ccbb3fb4e7518880e7e8d5c92ec
Adrian-Manuel <dasilvaadrian798@gmail.com>
2025-04-22 09:37:54 +0200

    security filter Chain
    cadena de filtro basica implementada

--- src/main/java/com/SmartPadel/spvendingManagerApi/security/config/SecurityConfig.java
+++ src/main/java/com/SmartPadel/spvendingManagerApi/security/config/SecurityConfig.java
@@ -1,4 +1,21 @@
 package com.SmartPadel.spvendingManagerApi.security.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain (HttpSecurity http) throws Exception{
+       return http.authorizeHttpRequests((requests)-> requests.anyRequest().authenticated())
+               .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+               .httpBasic(Customizer.withDefaults())
+               .build();
+    }
 }
