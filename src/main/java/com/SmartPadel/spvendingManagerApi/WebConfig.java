90c1679e9cccd9ce62450a13d26ba22b53dcf9b1
Adrian-Manuel <dasilvaadrian798@gmail.com>
2025-04-23 09:02:13 +0200

    Update WebConfig.java

--- src/main/java/com/SmartPadel/spvendingManagerApi/WebConfig.java
+++ src/main/java/com/SmartPadel/spvendingManagerApi/WebConfig.java
@@ -6,8 +6,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/")
-                .allowedOrigins("http://localhost:3000") // o el puerto de tu front
-                .allowedMethods("*");}
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");}
 
 }
