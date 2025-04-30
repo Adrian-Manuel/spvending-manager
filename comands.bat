52e1e214066ecbba6214704e31e4bf1e4822325f
Adrian-Manuel <dasilvaadrian798@gmail.com>
2025-04-30 13:46:02 +0200

    ci en gitlab registry container

--- comands.bat
+++ comands.bat
@@ -1,14 +1,8 @@
 
 git tag -d v1.0.0
-
 git push localgit --delete v1.0.0
-
 git add .
-
 git commit -m "ci en gitlab registry container"
-
 git push localgit main
-
 git tag v1.0.0
-
 git push localgit v1.0.0
