354baf526ccecccae754818ded5ffd6d6ad6711f
Adrian-Manuel <dasilvaadrian798@gmail.com>
2025-05-02 11:22:16 +0200

    prueba de ci cd

--- comands.bat
+++ comands.bat
@@ -1,7 +1,7 @@
-git tag -d v1.0.0
-git push fedoragit --delete v1.0.0
+git tag -d v1.0.1
+git push fedoragit --delete v1.0.1
 git add .
 git commit -m "prueba de ci cd"
 git push fedoragit main
-git tag v1.0.0
-git push fedoragit v1.0.0
+git tag v1.0.1
+git push fedoragit v1.0.1
