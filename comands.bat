4624da031fbf942a5312e54254b9fa4dd380ad79
Adrian-Manuel <dasilvaadrian798@gmail.com>
2025-05-02 16:00:32 +0200

    prueba de ci cd

--- comands.bat
+++ comands.bat
@@ -1,8 +1,27 @@
+@echo off
 set VERSION=v1.0.0
+set MENSAJE="prueba de ci cd"
+
+echo Eliminando tag local %VERSION%
 git tag -d %VERSION%
+
+echo Eliminando tag remoto %VERSION%
 git push fedoragit --delete %VERSION%
+
+echo Añadiendo cambios a staging
 git add .
-git commit -m "prueba de ci cd"
+
+echo Commiteando cambios
+git commit -m %MENSAJE%
+
+echo Pushing a la rama main
 git push fedoragit main
+
+echo Creando tag local %VERSION%
 git tag %VERSION%
+
+echo Pushing tag remoto %VERSION%
 git push fedoragit %VERSION%
+
+echo Listo. Se ha creado y subido la versión %VERSION%.
+pause
