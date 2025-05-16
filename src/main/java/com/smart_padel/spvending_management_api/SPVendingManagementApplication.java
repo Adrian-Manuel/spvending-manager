9b78a78f8b609be9cee2ed86b434b888c2b6033b
Adrian-Manuel <dasilvaadrian798@gmail.com>
2025-05-16 14:19:27 +0200

    prueba de ci cd

--- src/main/java/com/smart_padel/spvending_management_api/SPVendingManagementApplication.java
+++ src/main/java/com/smart_padel/spvending_management_api/SPVendingManagementApplication.java
@@ -2,10 +2,7 @@ package com.smart_padel.spvending_management_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
@@ -16,23 +13,5 @@ public class SPVendingManagementApplication {
 		SpringApplication.run(SPVendingManagementApplication.class, args != null ? args : new String[]{});
 
 	}
-
-<<<<<<< Updated upstream
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:5173")
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-						.allowCredentials(true);
-
-			}
-		};
-	}
-=======
-
->>>>>>> Stashed changes
 }
 
