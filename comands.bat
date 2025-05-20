3a95bbfea4e51d11ecbcbd0f15a01f74c60bb22d
Adrian-Manuel <dasilvaadrian798@gmail.com>
2025-05-20 15:29:50 +0200

    prueba ci cd 1

--- comands.bat
+++ comands.bat
@@ -1,12 +1,12 @@
 @echo off
-set VERSION=v1.0.
+set TAG=asd
 set MENSAJE="prueba ci cd 1"
 set REMOTE=localgit
-echo Eliminando tag local %VERSION%
-git tag -d %VERSION%
+echo Eliminando tag local %TAG%
+git tag -d %TAG%
 
-echo Eliminando tag remoto %VERSION%
-git push %REMOTE% --delete %VERSION%
+echo Eliminando tag remoto %TAG%
+git push %REMOTE% --delete %TAG%
 
 echo Añadiendo cambios a staging
 git add .
@@ -14,16 +14,16 @@ git add .
 echo Commiteando cambios
 git commit -m %MENSAJE%
 
-echo Creando tag local %VERSION%
-git tag %VERSION%
+echo Creando tag local %TAG%
+git tag %TAG%
 
 echo Pushing a la rama main
 git push %REMOTE% main
 
-echo Pushing tag remoto %VERSION%
-git push %REMOTE% %VERSION%
+echo Pushing tag remoto %TAG%
+git push %REMOTE% %TAG%
 
-echo Listo. Se ha creado y subido la versión %VERSION%.
+echo Listo. Se ha creado y subido la versión %TAG%.
 pause
 
 
