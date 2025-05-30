0a887d3a2969c718951bb7359c107c3c0b85a0e7
Adrian-Manuel <dasilvaadrian798@gmail.com>
2025-05-30 15:45:50 +0200

    configuracion de cors

--- src/main/java/com/smart_padel/spvending_management_api/SPVendingManagementApplication.java
+++ src/main/java/com/smart_padel/spvending_management_api/SPVendingManagementApplication.java
@@ -1,13 +1,33 @@
 package com.smart_padel.spvending_management_api;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class SPVendingManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SPVendingManagementApplication.class, args != null ? args : new String[]{});
+	}
+
+	@Value("${allowed.origin:http://localhost:80}")
+	String allowedOrigin;
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**")
+						.allowedOrigins(allowedOrigin)
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowCredentials(true);
+			}
+		};
 	}
 }
 
