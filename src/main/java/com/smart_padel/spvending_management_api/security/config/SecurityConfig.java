4316cd476bde89d23a80b33137d562f289dc235c
Adrian-Manuel <dasilvaadrian798@gmail.com>
2025-05-30 19:32:12 +0200

    cambios en docker compose

--- src/main/java/com/smart_padel/spvending_management_api/security/config/SecurityConfig.java
+++ src/main/java/com/smart_padel/spvending_management_api/security/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -52,6 +53,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req ->
+
                         req.requestMatchers("/api/v1/auth/**", "/swagger-ui.html", "/swagger-ui/**","/v3/api-docs", "/v3/api-docs/**", "/v3/api-docs.yaml").permitAll()
                                 .anyRequest().authenticated()
 
