de08483caac2fb2801cfa2329db5052a910c41ff
Adrian-Manuel <dasilvaadrian798@gmail.com>
2025-06-02 08:48:25 +0200

    prueba CI/CD

--- src/main/java/com/smart_padel/spvending_management_api/security/config/SecurityConfig.java
+++ src/main/java/com/smart_padel/spvending_management_api/security/config/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:80", "http://localhost:3000", "http://localhost"));
+        configuration.setAllowedOrigins(List.of("http://localhost:80", "http://localhost:3000", "http://localhost", "http://172.28.0.4:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
